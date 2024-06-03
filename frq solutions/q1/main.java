635d7883e2b64dccf7607c5717db0766e9aa963f
leoz30 <leoz30@nycstudents.net>
2024-06-03 09:36:04 -0400

    Update main.java

--- frq solutions/q1/main.java
+++ frq solutions/q1/main.java
@@ -1,1 +1,8 @@
-
+public class Main {
+  public static void main(String[] args) {
+    Feeder f1 = new Feeder(500);
+    f1.simulateOneDay(12);
+    System.out.println(f1.getCurrentFood());
+    // won't have a specific outcome due to Math.random()
+  }
+}
